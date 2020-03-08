b5f9553e3bc7043ac9a2892a7cd8162661f1038e
Jenny <truong.a.jenny@gmail.com>
2020-03-08 00:33:00 -0500

    :tada:

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from pip._vendor.distlib.compat import raw_input
 
-print ('hello friend!')
+print ("It's Brunch Time!! Let me help you find the perfect place!")
 name = raw_input("Enter a zip code: ")
 date = raw_input("What day would you like to search hours for? ")
 color = raw_input("Do you want to also search for bottomless drink deals? ")
