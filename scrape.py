93fc03b7a8f9c89f640e7f072c7b4890431ef812
Jenny <truong.a.jenny@gmail.com>
2020-03-22 14:34:51 -0400

    :tada: scraping works now but not outputting mins

--- scrape.py
+++ scrape.py
@@ -3,24 +3,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+url = 'https://orlandoairports.net/'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
-print(results.prettify())
+wait_time = soup.find("div", {"class": 'wait-time'}).get_text()
+result = soup.find("h6")
+#print(soup.prettify())
+#print(wait_time.strip())
+print(result.text)
 
+"""
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text + company_elem.text)
+    print("\n")
+    print(location_elem.text) 
+"""
+   
 
-
-""" 
-def get_website(url):
-    #Attemptes to get content at url with HTTP get req and return content
-    page = requests.get('https://orlandoairports.net/')
-    soup = BeautifulSoup(page.content, 'html5lib')
-    # print(soup)
-    wait_time = []
-    # soup.find()
-    return;
- """
 
 def log_error(e):
     """Prints out error if occurs"""
