912839223f8816890ac19bf63e6fb6d24e91bb16
Jenny <truong.a.jenny@gmail.com>
2020-03-23 15:45:11 -0400

    minutes aren't showing; need to fix

--- scrape.py
+++ scrape.py
@@ -7,23 +7,9 @@ url = 'https://orlandoairports.net/'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 wait_time = soup.find("div", {"class": 'wait-time'}).get_text()
-result = soup.find("h6")
-#print(soup.prettify())
-#print(wait_time.strip())
-print(result.text)
-
-"""
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
-    if None in (title_elem, company_elem, location_elem):
-        continue
-    print(title_elem.text + company_elem.text)
-    print("\n")
-    print(location_elem.text) 
-"""
-   
-
+#result = soup.find("span", {"class": 'gate1'}).get_text()
+print(wait_time.strip())
+#print (result)
 
 def log_error(e):
     """Prints out error if occurs"""
